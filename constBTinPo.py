3e469d69066029fb0637f7a30d7b58d29f2ff550
LEGEND0WSKI <siddhesh.koli32@gmail.com>
2024-11-14 21:26:27 -0600

    1/2 done

---
+++ constBTinPo.py
@@ -0,0 +1,26 @@
+# // Time Complexity :O(n) for node traversal
+# // Space Complexity :O(n) for recusive stack space and hashmap
+# // Did this code successfully run on Leetcode :Yes
+# // Any problem you faced while coding this :No
+
+
+# // Your code here along with comments explaining your approach
+
+class Solution:
+    def buildTree(self, inorder: list[int], postorder: list[int]) -> Optional[TreeNode]:
+        inorderIdx = {j:i for i,j in enumerate(inorder)}
+        
+        def helper(start,end):
+
+            if start > end:                     # pointer crosses end
+                return None
+            
+            root = TreeNode(postorder.pop())    # pop bottom element of preorder since last element is node
+            idx = inorderIdx[root.val]          # find popped value index in hashmap
+
+            
+            root.right = helper(idx+1,end)      # recursion left
+            root.left = helper(start,idx-1)     # recursion left
+            return root
+
+        return helper(0,len(inorder)-1)
